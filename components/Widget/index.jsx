61aa1a2a8ad2cca5f754e0b49a46088d79a0d654
giddyPwild <giddygbenga4@gmail.com>
2023-03-26 02:50:44 +0100

    march 26 commit

--- components/Widget/index.jsx
+++ components/Widget/index.jsx
@@ -28,13 +28,13 @@ const index = ({ trendingResults, followResults }) => {
 
             <div className="text-[#d9d9d9] space-y-3 bg-[#15181c] pt-2 rounded-xl w-11/12 xl:w-9/12">
                 <h4 className="font-bold text-xl px-4">Who to follow</h4>
-                {followResults.map((result, index) => (
+                {followResults?.map((result, index) => (
                     <div
                         className="hover:bg-white hover:bg-opacity-[0.03] px-4 py-2 cursor-pointer transition duration-200 ease-out flex items-center"
                         key={index}
                     >
                         <Image
-                            src={result.userImg}
+                            src={result?.userImg}
                             width={50}
                             height={50}
                             objectFit="cover"
--- pages/[id].jsx
+++ pages/[id].jsx
@@ -99,21 +99,59 @@ const PostPage = ({ trendingResults, followResults, providers }) => {
 export default PostPage;
 
 export async function getServerSideProps(context) {
-    const trendingResults = await fetch("https://www.jsonkeeper.com/b/NKEV").then(
-        (res) => res.json()
-    );
-    const followResults = await fetch("https://www.jsonkeeper.com/b/WWMJ").then(
-        (res) => res.json()
-    );
+    // const trendingResults = await fetch("https://www.jsonkeeper.com/b/NKEV").then(
+    //   (res) => res.json()
+    // );
+    const trendingResults = [
+      {
+        'heading': 'T20 World Cup 2021 * Live',
+        'description': 'NZvAUS: New Zealand and Australia clash in the T20 World Cup final',
+        'img': 'https://rb.gy/d9yjtu',
+        'tags': ['#T20WorldCupFinal, ', 'Kane Williamson']
+      },
+      {
+        'heading': 'Trending in United Arab Emirates',
+        'description': '#earthquake',
+        'img': 'https://rb.gy/jvuy4v',
+        'tags': ['#DubiaAirshow, ', 'gessdubai']
+      },
+      {
+        'heading': 'Trending in Digital Creators',
+        'description': 'tubbo and quackity',
+        'img': '',
+        'tags': ['QUACKITY AND TUBBO,']
+      },
+    ];
+  
+    // const followResults = await fetch("https://www.jsonkeeper.com/b/WWMJ").then(
+    //   (res) => res.json()
+    // );
+    const followResults = [
+      {
+        'userName': 'T20 World Cup 2021 * Live',
+        'userImg': 'https://rb.gy/d9yjtu',
+        'tag': ['#T20WorldCupFinal, ', 'Kane Williamson']
+      },
+      {
+        'heading': 'Trending in United Arab Emirates',
+        'userImg': 'https://rb.gy/jvuy4v',
+        'tag': ['#DubiaAirshow, ', 'gessdubai']
+      },
+      {
+        'heading': 'Trending in Digital Creators',
+        'userImg': 'https://rb.gy/jvuy4v',
+        'tag': ['QUACKITY AND TUBBO,']
+      },
+    ];
     const providers = await getProviders();
     const session = await getSession(context);
-
+  
     return {
-        props: {
-            trendingResults,
-            followResults,
-            providers,
-            session,
-        },
+      props: {
+        trendingResults,
+        followResults,
+        providers,
+        session,
+      },
     };
-}
+  }
